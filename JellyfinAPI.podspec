89d866de572412ff3d62f75ecad24565a4a6c3d6
PangMo5 <a3457709@icloud.com>
2021-06-05 06:42:27 +0900

    WIP

--- JellyfinAPI.podspec
+++ JellyfinAPI.podspec
@@ -1,15 +1,14 @@
 Pod::Spec.new do |s|
-  s.name = 'JellyfinAPI'
-  s.ios.deployment_target = '9.0'
-  s.osx.deployment_target = '10.11'
-  s.tvos.deployment_target = '9.0'
-  s.watchos.deployment_target = '3.0'
-  s.version = '10.7.6'
-  s.source = { :git => 'git@github.com:OpenAPITools/openapi-generator.git', :tag => 'v10.7.6' }
-  s.authors = 'OpenAPI Generator'
-  s.license = 'Proprietary'
-  s.homepage = 'https://github.com/OpenAPITools/openapi-generator'
-  s.summary = 'JellyfinAPI Swift SDK'
-  s.source_files = 'Sources/JellyfinAPI/**/*.swift'
-  s.dependency 'AnyCodable-FlightSchool', '~> 0.6.0'
+    s.source_files = '*.swift'
+    s.name = 'JellyfinAPI'
+    s.authors = 'Yonas Kolb'
+    s.summary = 'A generated API'
+    s.version = '10.7.6'
+    s.homepage = 'https://github.com/yonaskolb/SwagGen'
+    s.source = { :git => 'git@github.com:https://github.com/yonaskolb/SwagGen.git' }
+    s.ios.deployment_target = '9.0'
+    s.tvos.deployment_target = '9.0'
+    s.osx.deployment_target = '10.9'
+    s.source_files = 'Sources/**/*.swift'
+    s.dependency 'Alamofire', '~> 4.9.0'
 end
--- Package.swift
+++ Package.swift
@@ -1,33 +1,18 @@
-// swift-tools-version:5.1
+// swift-tools-version:4.0
 
 import PackageDescription
 
 let package = Package(
     name: "JellyfinAPI",
-    platforms: [
-        .iOS(.v9),
-        .macOS(.v10_11),
-        .tvOS(.v9),
-        .watchOS(.v3),
-    ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "JellyfinAPI",
-            targets: ["JellyfinAPI"]
-        ),
+        .library(name: "JellyfinAPI", targets: ["JellyfinAPI"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/Flight-School/AnyCodable", .exact("0.6.0")),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .exact("4.9.0")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "JellyfinAPI",
-            dependencies: ["AnyCodable", ],
-            path: "Sources/JellyfinAPI"
-        ),
+        .target(name: "JellyfinAPI", dependencies: [
+          "Alamofire",
+        ], path: "Sources")
     ]
 )
